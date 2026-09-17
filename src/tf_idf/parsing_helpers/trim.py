d58ac27b988ec147d54510f394dff6aed4910915
LukasAnell <lukasmanell@gmail.com>
2026-09-17 18:26:28 -0400

    trim.py, just with some notes on how I'm planning to trim each book

---
+++ src/tf_idf/parsing_helpers/trim.py
@@ -0,0 +1,18 @@
+# Trim before `When on board H.M.S. ‘Beagle,’ as naturalist, I was much struck with`
+# Trim after `and are being, evolved.`
+
+
+# Trim before `The nature of the following work will be best understood by a brief`
+# Trim after `probability to the action of sexual selection.`
+
+
+# Trim before `IT may be safely assumed that, two thousand years ago, before Caesar` (11-12 lines before this?)
+# Trim after `"Childe Roland to the dark Tower came."`
+
+
+# Trim before `Two aspects of animal life impressed me most during the journeys which I` (3-4 lines before this?)
+# Trim after `evolution of our race.`
+
+
+# Trim before `THE DATA OF ETHICS.`
+# Trim after `Transcriber's note:`
