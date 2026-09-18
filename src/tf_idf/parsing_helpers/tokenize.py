ed4ecd09df4b3c9e52448d26e36faf16651fc23e
LukasAnell <lukasmanell@gmail.com>
2026-09-18 01:47:46 -0400

    Implement tokenize(), which stores labeled, normalized tokens as pandas parquets

--- src/tf_idf/parsing_helpers/tokenize.py
+++ src/tf_idf/parsing_helpers/tokenize.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+from pandas import DataFrame
 
-def tokenize(book_id: int) -> None:
+
+def tokenize(book_id: str) -> None:
     # Root folder
     PROJECT_ROOT: Path = Path(__file__).resolve().parent
 
@@ -9,20 +11,42 @@ def tokenize(book_id: int) -> None:
     normalized_data_dir: Path = (
         PROJECT_ROOT.parents[2] / "data" / "processed" / "normalized"
     )
-    tokenized_data_dir: Path = PROJECT_ROOT.parents[2] / "data" / "processed" / "tokenized"
+    tokenized_data_dir: Path = (
+        PROJECT_ROOT.parents[2] / "data" / "processed" / "tokenized"
+    )
 
     # Create directory
     tokenized_data_dir.mkdir(parents=True, exist_ok=True)
 
+    with open(
+        normalized_data_dir / f"{book_id}.txt.utf-8", "r", encoding="utf-8"
+    ) as infile:
+        # Read in whole file as text
+        text: str = infile.read()
+
+        # Split over whitespace so it's a list where each entry is one word
+        tokens: list[str] = text.split()
+
+        # Turn list of tokens into a list of dicts that label each token as coming from the given book_id
+        rows: list[dict[str, str]] = [
+            {"book_id": book_id, "token": token} for token in tokens
+        ]
+
+        # Create a pandas DataFrame using tokens
+        df: DataFrame = DataFrame(rows)
+
+        # Create parquet file to store tokens on disk
+        df.to_parquet(tokenized_data_dir / f"{book_id}.parquet")  # type: ignore
+
 
 if __name__ == "__main__":
     # List of book ids
-    ids: list[int] = [
-        1228,
-        2300,
-        2940,
-        46129,
-        4341,
+    ids: list[str] = [
+        "1228",
+        "2300",
+        "2940",
+        "46129",
+        "4341",
     ]
 
     for book_id in ids:
