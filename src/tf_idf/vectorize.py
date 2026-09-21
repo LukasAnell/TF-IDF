d2acff253ae8251a12b42152cc24d91ffad469fc
LukasAnell <lukasmanell@gmail.com>
2026-09-20 23:38:39 -0400

    Update type hinting

--- src/tf_idf/vectorize.py
+++ src/tf_idf/vectorize.py
@@ -1,9 +1,12 @@
-def tf(t, d) -> None:
+from pandas import DataFrame
+
+
+def tf(t: str, d: DataFrame) -> None:
     # tf(t, d) = \frac{f(t, d)}{\sum_{t' \in d}{f(t', d)}}
     pass
 
 
-def idf(t, D) -> None:
+def idf(t: str, D: list[DataFrame]) -> None:
     # idf(t, D) = \log{\frac{N_D}{1 + n_t}}
     pass
 
